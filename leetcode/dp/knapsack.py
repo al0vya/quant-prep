8dc38d5004b27d0de2aaf2d757590b65c5e9fa4b
Alovya Chowdhury <alovya.chowdhury@gmail.com>
2024-07-24 01:38:36 +0100

    revise knapsack

--- leetcode/dp/knapsack.py
+++ leetcode/dp/knapsack.py
@@ -1,28 +1,34 @@
-def get_maximum_profit(
-    weights,
-    profits,
-    capacity,
-    i,
-    max_profit
-):
-    num_items = len(weights)
-    
-    if i == num_items: # if gone past final item
-        return max_profit 
-    elif weights[i] > capacity: # if current item doesn't fit, go to the next item while keeping our current profit
-        return get_maximum_profit(weights, profits, capacity, i+1, max_profit)
-    else: # if current item fits, add it and continue but also traverse another path where we try without it
-        return max(
-            get_maximum_profit(weights, profits, capacity, i+1, max_profit),
-            get_maximum_profit(weights, profits, capacity-weights[i], i+1, max_profit+profits[i])
-        )
+v = [1,5,3,8]
+w = [2,1,5,3]
+C = 6
+n = len(v)
+
+memo = [[None] * (C+1) for i in range(n)] # n x (C+1) matrix; need to hold 0 - 6 for the C state which is 7 states, not 6
+def knapsack_recursive(i, C):
+    if i == n:
+        return 0
         
-print(
-    get_maximum_profit(
-        weights=[2, 3, 1, 4],
-        profits=[4, 5, 3, 7],
-        capacity=5,
-        i=0,
-        max_profit=0
-    )
-)
+    if memo[i][C]:
+        return memo[i][C]
+    
+    inc = 0 if C-w[i] < 0 else v[i] + knapsack_recursive(i+1, C-w[i])
+    exc = knapsack_recursive(i+1, C)
+    
+    memo[i][C] = max(inc, exc)
+    return memo[i][C]
+
+print(knapsack_recursive(0, C))
+
+def knapsack_iterative():
+    # (n+1) x (C+1) state space table
+    # n+1 rows to hold the (n+1)th base case of zero
+    # need to hold 0 - 6 for the C state which is 7 states, not 6
+    table = [[0] * (C+1) for i in range(n+1)] 
+    for i in range(n-1,-1,-1):
+        for c in range(C+1):
+            inc = 0 if c-w[i] < 0 else v[i] + table[i+1][c-w[i]]
+            exc = table[i+1][c]
+            table[i][c] = max(inc, exc)
+    return table[0][C]
+    
+print(knapsack_iterative())
